947c95f746178da0f7082ba41db5eb41da7fff87
kszlak <43907643+kszlak@users.noreply.github.com>
2018-10-13 20:56:53 +0200

    Delete dict.py

--- dict.py
+++
@@ -1,7 +0,0 @@
-trening = {"cwiczenie": "bieganie",
-            "czas": 60,
-            "ilosc": 4,
-            "miejsce": "fitnessacademy"}
-
-for s in trening:
-    print("{0} : {1}".format(s, trening[s]))
