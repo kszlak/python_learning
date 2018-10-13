86e3f6d7dca36692e5ec0f22a98609ece72f611f
kszlak <kszlak@users.noreply.github.com>
2018-10-13 19:52:09 +0200

    Working version

--- listy2.py
+++ listy2.py
@@ -1,11 +1,12 @@
-baza = {"volvo": 1.8,
+baza = {"volvo": 7.8,
         "bmw": 4.0,
-        "fiat": 1.5,
+        "fiat": 6.5,
         "mercedes": 5.0}
 
-marka + raw_input('Podaj marke')
-podaj_km = 100
+marka = raw_input('Podaj marke: ')
+km = raw_input('Ile km: ')
+il = 100.0
 
 if marka in baza:
-    spalanie = baza[marka]
-    print(S"Ile na 100: " + spalanie)
+    spalanie = baza[marka]*(float(km)/100)
+    print("Spalil: " + str(spalanie))
