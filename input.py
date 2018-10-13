0e70f1a0c51bc139bf786ed767830da8689d50bd
kszlak <kszlak@users.noreply.github.com>
2018-10-13 19:17:28 +0200

    Add break

--- input.py
+++ input.py
@@ -1,10 +1,20 @@
+#print last character and print all names
+
 tab =[]
+tab2=[]
 
 while True:
-    x = raw_input ("Podaj nazwe: ")
-    print x
-    tab.append(x)
-    for s in tab:
-        if len(s)>0:
-            ostatnia_litera = s[-1]
-            print (ostatnia_litera)
+    x = raw_input ("Give a name: ")
+
+    if x=="no":
+        break
+
+    else:
+        tab.append(x)
+        for s in tab:
+            if len(s)>0:
+                ostatnia_litera = s[-1]
+                print (ostatnia_litera)
+
+for z in tab:
+    print z
