3346326885afef288d861c1a494e9129fbf77947
kszlak <kszlak@users.noreply.github.com>
2018-10-13 19:29:53 +0200

    Add function

--- listy.py
+++ listy.py
@@ -1,18 +1,10 @@
-cars = ['Peugeot', 'Fiat', 'Volvo']
-print (cars[0])
-print (cars[1])
+#dodanie funkcji
 
-for x in cars:
-    print x
+def print_tab(a):
+    for x in range (len(cars)):
+        print ("Index: " + str(x) + " : " + cars[x])
 
-for x in range (len(cars)):
-    print ("Index: " + str(x) + " : " + cars[x] )
+if __name__=="__main__":
+    cars = ['Peugeot', 'Fiat', 'Volvo']
 
-l = cars[2]
-for x in cars[2:3]:
-    print(x)
-
-#w python string dziala z indeksem
-
-nazwa = "Honda"
-print (nazwa[:2])
+print_tab(cars)
