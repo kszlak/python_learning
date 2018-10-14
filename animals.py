465b9177d7d542a068d120b72ea738b949f86ba5
kszlak <kszlak@users.noreply.github.com>
2018-10-14 13:28:43 +0200

    Add functions

--- animals.py
+++ animals.py
@@ -1,17 +1,26 @@
 #read from file
-file = open("animals.txt", "r")
-file2 = file.read()
-file3 = file2.split("\n")
-del file3[-1]
-print file3
+def get_dane():
+    file = open("animals.txt", "r")
+    file2 = file.read()
+    file3 = file2.split("\n")
+    del file3[-1]
+    return file3
 
+    #Alternative:
+    #for l in file:
+    #    file.append(l.strip())
+    #file.close()
 
-while True:
-    x = raw_input("Do you want add an animal? ")
-    if x == "yes":
-        y = raw_input("Add an animal: ")
-        file3.append(y)
-    if x =="no":
-        break
-print file3
-print file2
+def main():
+    lista = get_dane()
+    while True:
+        x = raw_input("Do you want add an animal? ")
+        if x == "yes":
+            y = raw_input("Add an animal: ")
+            lista.append(y)
+        if x =="no":
+            break
+    print lista
+
+if __name__== "__main__":
+    main()
