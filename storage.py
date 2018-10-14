d33d2a385f2cd25022909ce68efcc8f1844019fd
kszlak <kszlak@users.noreply.github.com>
2018-10-14 09:58:14 +0200

    Add functions

--- storage.py
+++ storage.py
@@ -1,24 +1,29 @@
 
-storage = [
-        {'nazwa':'cukier','id':1, 'kg':10, 'ilosc':2, 'cena':3.10},
-        {'nazwa':'maka','id':2, 'kg':10, 'ilosc':2, 'cena':3.10},
-        {'nazwa':'maslo','id':3, 'kg':20, 'ilosc':4, 'cena':3.10},
-        {'nazwa':'kasza','id':4, 'kg':10, 'ilosc':2, 'cena':5.10},
-        {'nazwa':'mleko','id':5, 'kg':10, 'ilosc':2, 'cena':3.10},
-        ]
+def get_dane():
+    return [
+            {'nazwa':'cukier','id':1, 'kg':10, 'ilosc':2, 'cena':3.10},
+            {'nazwa':'maka','id':2, 'kg':10, 'ilosc':2, 'cena':3.10},
+            {'nazwa':'maslo','id':3, 'kg':20, 'ilosc':4, 'cena':3.10},
+            {'nazwa':'kasza','id':4, 'kg':10, 'ilosc':2, 'cena':5.10},
+            {'nazwa':'mleko','id':5, 'kg':10, 'ilosc':2, 'cena':3.10},
+            ]
 
-ilosc_storage=0
-waga_storage=0
-cena_storage=0
-wybory=['kasza','maslo']
+def main():
+    ilosc_storage=0
+    waga_storage=0
+    cena_storage=0
+    wybory=['kasza','maslo']
 
-for s in storage:
-    if s['nazwa'] in wybory:
-        ilosc_storage = ilosc_storage + s['ilosc']
-        waga_storage = waga_storage + s['kg']
-        cena_storage = cena_storage + s['cena']
+    storage =get_dane()
 
+    for s in storage:
+        if s['nazwa'] in wybory:
+            ilosc_storage = ilosc_storage + s['ilosc']
+            waga_storage = waga_storage + s['kg']
+            cena_storage = cena_storage + s['cena']
+    print ('ilosc: '+ str(ilosc_storage))
+    print ('waga: '+ str(waga_storage))
+    print ('cena: '+ str(cena_storage))
 
-print ('ilosc: '+ str(ilosc_storage))
-print ('waga: '+ str(waga_storage))
-print ('cena: '+ str(cena_storage))
+if __name__ == "__main__":
+    main()
