43c8bcbb89a978e1334d85ee5d2e750b1734f54a
kszlak <kszlak@users.noreply.github.com>
2018-10-14 12:13:01 +0200

    Working version

--- storage.py
+++ storage.py
@@ -3,7 +3,7 @@ def get_dane():
             {'nazwa':'cukier','id':1, 'kg':10, 'ilosc':2, 'cena':3.10},
             {'nazwa':'maka','id':2, 'kg':10, 'ilosc':2, 'cena':3.10},
             {'nazwa':'maslo','id':3, 'kg':20, 'ilosc':4, 'cena':3.10, 'toxic':True},
-            {'nazwa':'kasza','id':4, 'kg':10, 'ilosc':2, 'cena':5.10},
+            {'nazwa':'kasza','id':4, 'kg':10, 'ilosc':2, 'cena':5.10, 'toxic':False},
             {'nazwa':'mleko','id':5, 'kg':10, 'ilosc':2, 'cena':3.10, 'toxic':True},
             ]
 
@@ -12,6 +12,7 @@ def main():
     waga_storage=0
     cena_storage=0
     ilosc_tok=0
+    ilosc_nietok=0
     wybory=['kasza','maslo']
 
     storage =get_dane()
@@ -29,8 +30,16 @@ def main():
     for poz in storage:
         if "toxic" in poz:
             if poz['toxic']==True:
-                ilosc_tok=ilosc_tok+1
-    print("Ilosc toksycznych: " + str(ilosc_tok))
+                ilosc_tok = ilosc_tok+1
+    print("Ilosc toksycznych: " + str(ilosc_tok) + " Nazwa: " + poz['nazwa'])
+
+    for poz2 in storage:
+        if "toxic" in poz2:
+            if poz2['toxic']==False:
+                ilosc_nietok = ilosc_nietok+1
+            else:
+                ilosc_nietok = ilosc_nietok+1
+    print("Ilosc nietoksycznych: " + str(ilosc_nietok) + " Nazwa: " + poz['nazwa'])
 
 if __name__ == "__main__":
     main()
