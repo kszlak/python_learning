f5290d552e5e3f81026044759d2dcfe9bc4c8e59
kszlak <kszlak@users.noreply.github.com>
2018-10-30 22:00:40 +0100

    Add functions

--- cats.py
+++ cats.py
@@ -1,13 +1,21 @@
-tab=[]
-q=0
+def data_cats():
+    cats = []
+    while True:
+        ask = raw_input("Want to add a cat? y/n\n")
+        if ask == "y":
+            name = raw_input("Write name: \n")
+            cats.append(name)
+        else: break
+    return cats
 
-while True:
-    x = raw_input ("Do you want to add a cat? ")
-    if x == "yes":
-        y = raw_input ("Add a cat: ")
-        print y
-        tab.append(y)
-    elif x=="no":
-        for s in range (len(tab)):
-            print ("Kitty {0}: {1}".format(s, tab[s]))
-        break
+def main():
+    kitty = data_cats()
+    i = 0
+    print kitty
+    for s in range(len(kitty)):
+        i = i+1
+        print ("Cat {0}: {1}".format(i, kitty[s]))
+
+
+if __name__=="__main__":
+    main()
