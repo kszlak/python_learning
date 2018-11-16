0f71fb9f86586c47b48f0333f9da832b3b920ad2
kszlak <kszlak@users.noreply.github.com>
2018-11-16 20:59:42 +0100

    Add string calculator

--- unittest/test.py
+++ unittest/test.py
@@ -6,3 +6,10 @@ class TestCalcurator(unittest.TestCase):
 	def test_dodawanie(self):
 		r = calculate(1, 2, '+')
 		self.assertEqual(r, 3)
+
+from calculator import str_calculator
+
+class testStringCalculator(unittest.TestCase):
+	def test_concat(self):
+		r = str_calculator("a", "b", 'concat')
+		self.assertEqual(r, 'ab')
